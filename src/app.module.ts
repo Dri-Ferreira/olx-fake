2a37408a34609729b0c01e908bf74d15d82cc59d
Adriano hilario <adrianoferreira31@gmail.com>
2023-01-31 12:22:36 -0300

    :recycle: refactor code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { AdvertisementsModule } from './modules/advertisements/advertisements.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AdvertisementsModule, CategoriesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
