cd12f0614b5e3d828082b754b405fc7d1df58d8a
Adriano hilario <adrianoferreira31@gmail.com>
2023-01-31 12:21:18 -0300

    :construction: feat(user): implementing user method

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,18 +5,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UserService {
   create(createUserDto: CreateUserDto) {
-
-    1056+ 4356 + 721
     return 'This action adds a new user';
-    3188,80 + 721,6 + 528 = 4438,40
-
-    6133 pronto ferro  
-    2760 bloco 
-    800 areia 
-    600 brita 
-    1950 cimento 
-    400 pedra 
-    744 tabua 
   }
 
   findAll() {
