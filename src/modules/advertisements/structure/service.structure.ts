cf05c7fa3139e1a4f4204eed375d1c5c4983a629
Adriano hilario <adrianoferreira31@gmail.com>
2023-02-22 18:06:06 -0300

    :construction: creating the advertisement trimmings

--- src/modules/advertisements/structure/service.structure.ts
+++ src/modules/advertisements/structure/service.structure.ts
@@ -1,5 +1,10 @@
+import { createAdsBodyParams } from './../types/ads-params-types';
+import { Advertisements } from '@prisma/client';
 import { createAdsParams } from '../types/ads-params-types';
 
 export interface ICreateAdvertisementsService {
-  execute(params: createAdsParams): Promise<any>;
+  execute(
+    params: createAdsParams,
+    data: createAdsBodyParams,
+  ): Promise<Partial<Advertisements>>;
 }
--- src/modules/advertisements/types/ads-params-types.ts
+++ src/modules/advertisements/types/ads-params-types.ts
@@ -1,3 +1,16 @@
 export type createAdsParams = {
   id: string;
 };
+
+export type createAdsBodyParams = {
+  state: string;
+  category: string;
+  images: string;
+  dateCreated: Date;
+  title: string;
+  price: number;
+  priceNegotiable: boolean;
+  description: string;
+  views: number;
+  status: string;
+};
