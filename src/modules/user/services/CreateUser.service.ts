475639d5ba014da158ca7aae19542b376b8f252a
Adriano hilario <adrianoferreira31@gmail.com>
2023-03-09 16:15:29 -0300

    :recycle: refactoring the user service

--- src/modules/user/services/CreateUser.service.ts
+++ src/modules/user/services/CreateUser.service.ts
@@ -4,11 +4,12 @@ import { UserRepository } from '../respository/User.respository';
 import { ICreateUserService } from '../structures/service.structure';
 import { createUserParams } from '../types/user-params-types';
 import * as bcrypt from 'bcrypt';
+import { IUserRepository } from '../structures/repository.structure';
 
 Injectable();
 export class CreateUserService implements ICreateUserService {
   constructor(
-    @Inject(UserRepository) private readonly userRepository: UserRepository,
+    @Inject(UserRepository) private readonly userRepository: IUserRepository,
   ) {}
   async execute(params: createUserParams): Promise<Partial<User>> {
     const existsUser = await this.userRepository.findByEmail(params);
