bd4b8d40d9c7b8ec01efdda5489f5d6c324ff69f
Adriano hilario <adrianoferreira31@gmail.com>
2023-03-09 16:16:18 -0300

    :recycle: refactoring the  get  user id service

--- src/modules/user/services/GetUserId.service.ts
+++ src/modules/user/services/GetUserId.service.ts
@@ -3,10 +3,11 @@ import { Inject, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { UserRepository } from '../respository/User.respository';
 import { IGetUserIdService } from '../structures/service.structure';
+import { IUserRepository } from '../structures/repository.structure';
 Injectable();
 export class GetUserIdService implements IGetUserIdService {
   constructor(
-    @Inject(UserRepository) private readonly userRepository: UserRepository,
+    @Inject(UserRepository) private readonly userRepository: IUserRepository,
   ) {}
 
   async execute(params: GetIdParams): Promise<Partial<User>> {
