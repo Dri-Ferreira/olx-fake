dccabc4709119cccef36baed92866dd48102b3b8
Adriano hilario <adrianoferreira31@gmail.com>
2023-03-09 16:16:01 -0300

    :recycle: refactoring the  get all user service

--- src/modules/user/services/GetStatesAll.service.ts
+++ src/modules/user/services/GetStatesAll.service.ts
@@ -1,12 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { States } from '@prisma/client';
 import { UserRepository } from '../respository/User.respository';
+import { IUserRepository } from '../structures/repository.structure';
 import { IGetStatesService } from '../structures/service.structure';
 
 Injectable();
 export class GetStatesAllService implements IGetStatesService {
   constructor(
-    @Inject(UserRepository) private readonly userRepository: UserRepository,
+    @Inject(UserRepository) private readonly userRepository: IUserRepository,
   ) {}
   async execute(): Promise<Partial<States[]>> {
     return await this.userRepository.getAllStates();
