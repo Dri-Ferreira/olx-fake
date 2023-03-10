cc1dde05e99125ba4fa4b3615c1abc4c5b9cb94c
Adriano hilario <adrianoferreira31@gmail.com>
2023-03-10 09:51:22 -0300

    :sparkles: adding new search of user

--- src/modules/user/structures/repository.structure.ts
+++ src/modules/user/structures/repository.structure.ts
@@ -1,11 +1,11 @@
-import { IFindByEmail, GetIdParams } from './../types/user-params-types';
+import { GetIdParams } from './../types/user-params-types';
 import { States, User } from '@prisma/client';
 import { createUserParams } from '../types/user-params-types';
 
 export interface IUserRepository {
   create(params: createUserParams): Promise<User>;
   getAllStates(): Promise<Partial<States[]>>;
-  findByEmail(params: IFindByEmail): Promise<User | null>;
+  existUser(where: Partial<User> | null): Promise<User>;
   getAllUser(): Promise<Partial<User[]>>;
   getUserId(params: GetIdParams): Promise<Partial<User>>;
 }
